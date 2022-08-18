cbb256ac88417a03aabe10aac9eda66f592c57db
Serg <spka2008@yandex.ru>
2022-08-18 16:02:25 +0300

    Скрипт для форматированного вывода csv

---
+++ formatcsv.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import os
+import csv
+
+if (len(os.sys.argv) == 1):
+    os.system("cat > ggg")
+else:
+    os.system("cat "+os.sys.argv[1]+ " > ggg")
+wf=[]
+with open("ggg","r") as file:
+    reader=csv.reader(file)
+    rr=next(reader)
+    for item in rr:
+        wf.append(len(item))
+    for row in reader:
+        i=0
+        for item in row:
+            if (wf[i]<len(item)) : wf[i]=len(item)
+            i+=1
+file.close()
+with open("ggg","r") as file:
+    reader=csv.reader(file)
+    for row in reader:
+        i=0
+        for item in row:
+            format_xl= "{:"+str(wf[i])+"s} "
+            print(format_xl.format(item), end="|")  
+            i+=1
+        print("\n")
+os.remove("ggg")
+
