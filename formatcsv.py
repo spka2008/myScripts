eca3948090cc6cbc30f99250811af147a7186654
Serg <spka2008@yandex.ru>
2022-08-22 10:07:14 +0300

    Удаление временного файла при пустом входящем потоке format.csv

--- formatcsv.py
+++ formatcsv.py
@@ -7,26 +7,26 @@ if (len(os.sys.argv) == 1):
     os.system("cat > ggg")
 else:
     os.system("cat "+os.sys.argv[1]+ " > ggg")
-wf=[]
-with open("ggg","r") as file:
-    reader=csv.reader(file)
-    rr=next(reader)
-    for item in rr:
-        wf.append(len(item))
-    for row in reader:
-        i=0
-        for item in row:
-            if (wf[i]<len(item)) : wf[i]=len(item)
-            i+=1
-file.close()
-with open("ggg","r") as file:
-    reader=csv.reader(file)
-    for row in reader:
-        i=0
-        for item in row:
-            format_xl= "{:"+str(wf[i])+"s} "
-            print(format_xl.format(item), end="|")  
-            i+=1
-        print("\n")
+if (os.stat("ggg").st_size != 0):
+    wf=[]
+    with open("ggg","r") as file:
+        reader=csv.reader(file)
+        rr=next(reader)
+        for item in rr:
+            wf.append(len(item))
+        for row in reader:
+            i=0
+            for item in row:
+                if (wf[i]<len(item)) : wf[i]=len(item)
+                i+=1
+    with open("ggg","r") as file:
+        reader=csv.reader(file)
+        for row in reader:
+            i=0
+            for item in row:
+                format_xl= "{:"+str(wf[i])+"s} "
+                print(format_xl.format(item), end="|")  
+                i+=1
+            print("\n")
 os.remove("ggg")
 
