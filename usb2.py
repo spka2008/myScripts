c8e5f43d21bc78597bd5f6fd768e751f41db920a
serg <spka2008@yandex.ru>
2023-06-02 00:22:20 +0300

    usb with partless

--- usb2.py
+++ usb2.py
@@ -3,25 +3,32 @@
 from subprocess import check_output
 def attribute (s):
     item = check_output(['lsblk', '-lnpo', 'FSAVAIL,LABEL', s[0]], universal_newlines=True).split("\n")[0].strip()
-    item = item.split(' ')[0]
     s.append(item)
     s[0]=s[0].replace('/dev/','')
-    ss=s[2].split('/')
+    ss=s[1].split('/')
     if (len(ss) > 2):
-        s[2]=ss[len(ss)-1]
+        s[1]=ss[len(ss)-1]
+    return s
+def appendLines (s):
+    if (len(s)==1):
+        s.append('')
+    elif (len(s)>2):
+        del s[2:]
     return s
 def getoutputs (s):
-    if (s[2]==''):
+    if (s[1]==''):
         col="gray"
     else:
         col="green"
-    return "<span color='{}'><b>[{}]</b> </span><i>{}: </i>{}".format(col, s[0], s[2], s[3])
-lines = check_output(['lsblk', '-lpno', 'NAME,TYPE,MOUNTPOINT'], universal_newlines=True)
+    return "<span color='{}'><b>[{}]</b> </span><i>{}: </i>{}".format(col, s[0], s[1], s[2])
+lines = check_output(['lsblk', '-lpno', 'NAME,MOUNTPOINT'], universal_newlines=True)
 lines = lines.split("\n")
 lines = filter(lambda s: (s!='' and s.find('SWAP')==-1 and s.find('boot')==-1), lines)
-lines = list(map(lambda s: s.split(' '), lines))
+lines = list(map(lambda s: s.split(), lines))
 devices = check_output(['blkid', '-o', 'device'], universal_newlines=True).split('\n')
 lines = list(filter(lambda s: s[0] in devices, lines))
+lines = list(map(appendLines,lines))
+print(lines)
 lines = list(map(attribute ,lines))
 outputs=list(map(getoutputs, lines))
 output='|'.join(outputs)
